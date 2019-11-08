cbd61312090a63bb22c1a7849314c19b4a9a5ce9
Saramaria19 <57485769+Saramaria19@users.noreply.github.com>
2019-11-08 16:03:48 -0300

    add Projeto

---
+++ controleDeNotificacao/src/main/java/com/example/controleDeNotificacao/ControleDeNotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.controleDeNotificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeNotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeNotificacaoApplication.class, args);
+	}
+
+}
