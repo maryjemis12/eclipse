2cad24a6d79014b45525109dc8b5da3f2f840642
20161204010040 <20161204010040@LAJ291440.ifrn.local>
2019-11-26 10:41:30 -0200

    Clonando o projeto oficial

---
+++ controleNotificacao/src/test/java/com/controleDeNotificacao/controleDeNotificacao/ControleDeNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controleDeNotificacao.controleDeNotificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeNotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
